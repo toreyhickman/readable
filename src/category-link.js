d1bf6ec76d817aabd69839f258ae07426a8eac62
Torey Hickman <torey@toreyhickman.com>
2017-10-27 19:29:13 -0400

    feat: Define prop types

--- src/category-link.js
+++ src/category-link.js
@@ -1,8 +1,14 @@
 
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 class CategoryLink extends Component {
+  static PropTypes = {
+    name: PropTypes.string.isRequired,
+    path: PropTypes.string.isRequired
+  }
+
   render() {
     const { name, path } = this.props
 
--- src/category-menu.js
+++ src/category-menu.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import CategoryLink from "./category-link";
 
 
 class CategoryMenu extends Component {
+  static PropTypes = {
+    categories: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string,
+      path: PropTypes.string
+    })).isRequired
+  }
+
   render() {
     const { categories } = this.props
 
