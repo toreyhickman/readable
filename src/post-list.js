7c1fa4cae2dbac6a456ac6b6ab30eea3fee797b5
Torey Hickman <torey@toreyhickman.com>
2017-10-27 19:43:17 -0400

    feat: Sort posts by vote count

--- src/post-list.js
+++ src/post-list.js
@@ -1,20 +1,52 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 class PostList extends Component {
+  static PropTypes = {
+    posts: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      timestamp: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+      voteScore: PropTypes.number.isRequired,
+      deleted: PropTypes.bool.isRequired,
+      commentCount: PropTypes.number.isRequired
+    })).isRequired
+  }
+
+  state = {
+    sortOn: "voteScore"
+  }
 
   heading = () => "Posts"
 
-  render() {
-    const { posts } = this.props
+  sortedPosts = () => {
+    const { sortOn } = this.state
 
+    return this.props.posts.sort((a, b) => {
+      if (a[sortOn] < b[sortOn]) {
+        return 1
+      }
+
+      if (a[sortOn] > b[sortOn]) {
+        return -1
+      }
+
+      return 0
+    })
+  }
+
+  render() {
     return (
       <div>
         <h1>{this.heading()}</h1>
         <ul>
           {
-            posts.map(post => (
-              <li>{post.id}</li>
+            this.sortedPosts().map(post => (
+              <li key={post.id}>{post.id} score: {post.voteScore}</li>
             ))
           }
         </ul>
